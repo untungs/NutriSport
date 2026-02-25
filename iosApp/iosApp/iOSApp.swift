0de15e9ceae55cc01479006aaaacd86f075e13e2
Untung Suryono <untungs30@gmail.com>
2026-02-25 09:58:54 +0700

    integrate Google SignIn with firebase (iOS)

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,29 @@
+import Firebase
+import GoogleSignIn
 import SwiftUI
 
 @main
 struct iOSApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL(perform: { url in
+                    GIDSignIn.sharedInstance.handle(url)
+                })
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
