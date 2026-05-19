69eff07e76d37c42c8a1c0bdb03b0943c73518ec
Untung Suryono <untungs30@gmail.com>
2026-05-19 18:08:57 +0700

    ui(auth): handle edge-to-edge layout in AuthScreen

--- feature/auth/src/commonMain/kotlin/io/untungs/nutrisport/auth/AuthScreen.kt
+++ feature/auth/src/commonMain/kotlin/io/untungs/nutrisport/auth/AuthScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -42,6 +43,7 @@ fun AuthScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .systemBarsPadding()
             .padding(24.dp)
     ) {
         Column(
